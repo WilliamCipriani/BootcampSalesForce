f9e4bb2a2271c08014b54170816c7c9a2a3c678c
WilliamCipriani <wciprianinaveda@gmail.com>
2024-08-08 16:15:05 -0500

    actualizando proyecto

--- pages/api/generateUser.js
+++ pages/api/generateUser.js
@@ -2,10 +2,8 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
-    // Realizamos la petición a la API
     const { data } = await axios.get('https://randomuser.me/api/?results=10');
 
-    // Extraemos y mapeamos los datos necesarios
     const users = data.results.map(user => ({
       gender: user.gender,
       name: `${user.name.first} ${user.name.last}`,
@@ -15,7 +13,6 @@ export default async function handler(req, res) {
       picture: user.picture.medium,
     }));
 
-    // Enviamos los datos al frontend
     res.status(200).json(users);
   } catch (error) {
     console.error('Error al obtener usuarios:', error);
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 import { CiLocationOn } from "react-icons/ci";
 
 export default function Home() {
@@ -28,6 +29,7 @@ export default function Home() {
     const filtered = users.filter(user => 
       user.name.toLowerCase().includes(value)
     );
+
     setFilteredUsers(filtered);
   };
   
@@ -35,38 +37,39 @@ export default function Home() {
     return <p className="text-center text-gray-500">Cargando...</p>;
   }
 
-  if (filteredUsers.length === 0) {
-    return <p className="text-center text-gray-500">No se encontraron usuarios</p>;
-  }
-
   return (
     <div className="min-h-screen bg-gray-100 py-10">
       <h1 className="text-4xl font-bold text-center text-gray-600 mb-10">Lista de Usuarios</h1>
-      <div className="max-w-md mx-auto mb-6">
+      <div className="max-w-md mx-auto mb-6 relative">
+        <FaSearch className="absolute left-3 top-3 text-gray-400" />
         <input
           type="text"
           placeholder="Buscar por nombre..."
           value={searchTerm}
           onChange={handleSearch}
-          className="w-full p-3 border border-gray-300 rounded-lg shadow-sm"
+          className="w-full p-3 pl-10 border border-gray-300 rounded-lg shadow-sm"
         />
       </div>
-      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl mx-auto px-4">
-        {filteredUsers.map((user, index) => (
-          <div key={index} className="bg-white p-6 rounded-lg shadow-lg">
-            <div className="flex items-center justify-center">
-              <img src={user.picture} alt={user.name} className="w-24 h-24 rounded-full border-4 border-gray-500" />
+      {filteredUsers.length > 0 ? (
+        <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-7xl mx-auto px-4">
+          {filteredUsers.map((user, index) => (
+            <div key={index} className="bg-white p-6 rounded-lg shadow-lg">
+              <div className="flex items-center justify-center">
+                <img src={user.picture} alt={user.name} className="w-24 h-24 rounded-full border-4 border-gray-500" />
+              </div>
+              <div className="mt-4 text-center">
+                <h2 className="text-xl font-semibold text-gray-800">{user.name}</h2>
+                <p className="text-gray-600">{user.gender}</p>
+                <p className="text-gray-600 flex justify-center"><CiLocationOn className='mt-1'/>{user.location}</p>
+                <p className="text-gray-600 mt-2">{user.email}</p>
+                <p className="text-gray-600 mt-2">Fecha de Nacimiento: {new Date(user.dob).toLocaleDateString()}</p>
+              </div>
             </div>
-            <div className="mt-4 text-center">
-              <h2 className="text-xl font-semibold text-gray-800">{user.name}</h2>
-              <p className="text-gray-600">{user.gender}</p>
-              <p className="text-gray-600 flex justify-center"><CiLocationOn className='mt-1'/> {user.location}</p>
-              <p className="text-gray-600 mt-2">{user.email}</p>
-              <p className="text-gray-600 mt-2">Fecha de Nacimiento: {new Date(user.dob).toLocaleDateString()}</p>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-gray-500">No se encontraron usuarios</p>
+      )}
     </div>
   );
 }
